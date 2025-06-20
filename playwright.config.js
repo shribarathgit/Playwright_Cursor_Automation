42158647e05d83bb5315a07317fd6bcb74fb3df5
shribarath <shribarath.ganapathisubramanian@ideas2it.com>
2025-06-20 17:36:11 +0530

    config changes

--- playwright.config.js
+++ playwright.config.js
@@ -24,8 +24,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html'],
-    ['json', { outputFile: 'playwright-report/report.json' }]
+    ['html', { outputFolder: 'src/test-results', open: 'never' }],
+    ['json', { outputFile: 'src/test-results/report.json' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
